a3e4442f62ecff66257320cbcd247de08ca81330
MWest2020 <mwesterweel@hotmail.com>
2021-01-05 15:03:11 +0100

    repo for javascipt fundamental practice

---
+++ challenges Jonas Schmedtman.js
@@ -0,0 +1,55 @@
+"strict mode"
+
+// const markBMI = Math.round(calcBMI(78, 1.69) * 100) / 100;
+// const johnBMI = Math.round(calcBMI(95, 1.88) * 100) / 100;
+
+// function calcBMI(weight, height){
+//     return (weight / (height ** 2) );
+// }
+
+// let markHigherBMI = false;
+// const whichBMIisGreater = () => {
+    
+//     if (markBMI > johnBMI) {
+//         return markHigherBMI = true;
+//     } else {
+//         return markHigherBMI = false;
+//     }; 
+    
+// }
+// whichBMIisGreater(markBMI, johnBMI);
+
+// (markHigherBMI ? console.log( `Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`) : console.log( `John's BMI ${johnBMI} is higher than Mark's ${markBMI}!`))
+
+// const dolphinScore = [96,108, 9];
+// const koalaScore = [96, 108, 119];
+
+// const averageFromArr = (Array) => {
+//     let total = 0;
+//     for(i =0 ; i < Array.length; i++){
+//         total += Array[i];
+//     }  
+//     return total / Array.length + 1 ;
+// }
+
+// const averageDolphins = averageFromArr(dolphinScore);
+// const averageKoala = averageFromArr(koalaScore);
+
+// const higherTeamScore = () => {
+//     if(averageDolphins < 100 && averageKoala < 100 ){
+//         console.log(`Sadly, no team scored at least a 100 points...`)
+//     } else {
+//         if(averageDolphins > averageKoala){
+//             console.log(`The winners are the Dolphins!`)
+//         } else if (averageKoala > averageDolphins){
+//             console.log(`The winners are the Koalas!`)
+//         } else {
+//             console.log(`It's a draw!!`)
+//         }
+//     }
+// }
+
+// higherTeamScore();
+
+// console.log(averageDolphins, averageKoala);
+
